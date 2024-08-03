182fa4c661f74d8ce04f46f6e0c3269726cf90d6
butter2002 <86915035+butter2002@users.noreply.github.com>
2024-08-04 00:22:04 +0900

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catRouter = require('./routes/cat');
 var dogRouter = require('./routes/dog'); // dogRouter のインポート
-
+var notes_from_bRouter = require('./routes/notes_from_b');
 var app = express();
 
 // view engine setup
@@ -25,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cat', catRouter); // catRouter の設定
 app.use('/dog', dogRouter); // dogRouter の設定
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/cat.js
+++ routes/cat.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+const cors = require('cors');
+
+router.use(cors());
 
 router.get('/', async function(req, res, next) {
   try {
-    const response = await axios.get('https://api.thecatapi.com/v1/images/search');
+    const response = await axios.get('https://api.thecatapi.com/v1/images/search', {
+      headers: {
+        'x-api-key': 'YOUR_API_KEY' // 必要に応じてAPIキーを設定
+      }
+    });
     const data = response.data;
     const catImageUrl = data[0].url;
-    res.send(`<img src="${catImageUrl}" alt="Cat Image">`);
+    res.json([{ url: catImageUrl }]); // JSON形式でデータを返す
   } catch (error) {
-    next(error); // エラーハンドリングを追加
+    console.error(error); // コンソールにエラーメッセージを出力
+    next(error); // エラーハンドリング
   }
 });
 
