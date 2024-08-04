9d1ff3973b84d70355f2ad1c8009ad580820c137
butter2002 <86915035+butter2002@users.noreply.github.com>
2024-08-04 18:21:38 +0900

    commit

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -3,8 +3,13 @@ var router = express.Router();
 const cors = require('cors');
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://butter2002pp:MyG9dSyqnqE00yeo@test.ycp4yxb.mongodb.net/?retryWrites=true&w=majority&appName=test";
-const client = new MongoClient(uri);
+const uri = "mongodb+srv://butter2002pp:MyG9dSyqnqE00yeo@test.ycp4yxb.mongodb.net/?retryWrites=true&w=majority&appName=test&tls=true";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 30000, // 接続タイムアウトを30秒に設定
+  socketTimeoutMS: 45000   // ソケットタイムアウトを45秒に設定
+});
 
 // CORSミドルウェアを使用
 router.use(cors());
@@ -30,7 +35,11 @@ router.get('/', async (req, res) => {
     res.status(500).send('Internal Server Error');
   } finally {
     // クライアント接続を閉じる
-    await client.close();
+    try {
+      await client.close();
+    } catch (closeError) {
+      console.error('Error occurred while closing MongoDB connection:', closeError);
+    }
   }
 });
 
